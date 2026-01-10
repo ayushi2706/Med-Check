08506e6bdf5b7a51666e24d8ceeb4fdbbf065cdb
ayushi2706 <125763606+ayushi2706@users.noreply.github.com>
2026-01-10 16:29:08 -0500

    Update caregiver_dashboard.py

--- caregiver_dashboard.py
+++ caregiver_dashboard.py
@@ -11,13 +11,15 @@ class care_giver_dashboard():
         root.geometry("500x700")
         root.configure(bg="#2c3e50")
 
+        self.dose_taken_today = False
+
         #status display
         self.status_frame = tk.Frame(root, bg="red", height=150)
         self.status_frame.pack(fill="x")
         self.status_text = tk.Label(self.status_frame, text="MEDICATION LATE", fg="white", bg="red", font=("Arial", 24, "bold"))
         self.status_text.pack(pady=40)
 
-        #system Log
+        #system log
         tk.Label(root, text="System Log:", font=("Arial", 12, "bold")).pack(pady=10)
         self.log = tk.Text(root, height=12, width=50, bg="#f0f0f0")
         self.log.pack(padx=20, pady=5)
@@ -58,22 +60,34 @@ class care_giver_dashboard():
                     pass
 
     def record_dose(self):
-        timestamp = datetime.now().strftime("%H:%M:%S")
-        # Use .after() to update UI from a background thread safely
-        self.root.after(0, lambda: self.status_frame.config(bg="green"))
-        self.root.after(0, lambda: self.status_text.config(text="MEDICATION TAKEN", bg="green"))
-        self.root.after(0, lambda: self.log.insert(tk.END, f"[{timestamp}] - Camera Triggered: Meds Processed\n"))
 
-        self.save_to_file(timestamp)
+        #if meds not already taken 
+        if not self.dose_taken_today:
+            self.dose_taken_today = True
+            timestamp = datetime.now().strftime("%H:%M:%S")
+            
+            #ui update
+            self.status_frame.config(bg="green")
+            self.status_text.config(text="MEDICATION TAKEN", bg="green")
+            self.log.insert(tk.END, f"[{timestamp}] - Dose Detected & Processed\n")
+            
+            #disable test button to prevent spamming during demo
+            self.test_btn.config(state="disabled")
+            
+            self.save_to_file(timestamp) #save to log
+        else:
+            self.log.insert(tk.END, "Note: Additional motion ignored.\n")
 
     def save_to_file(self, time_string):
         with open("med_history.txt", "a") as file:
             file.write(f"Medication taken at: {time_string}\n")
             
     def reset_system(self):
+        self.dose_taken_today = False
+        self.test_btn.config(state="normal") #reenable button
         self.status_frame.config(bg="red")
         self.status_text.config(text="MEDICATION LATE", bg="red")
-        self.log.insert(tk.END, "--- System Reset for New Day ---\n")
+        self.log.insert(tk.END, f"--- System Reset: {datetime.now().strftime('%m/%d')} ---\n")
 
 if __name__ == "__main__":
     root = tk.Tk()
